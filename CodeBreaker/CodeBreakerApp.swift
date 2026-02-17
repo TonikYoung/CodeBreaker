bad692ed10c3c0287aeb68a14989556586c290ad
Abramov Anton <abramovanto@ozon.ru>
2026-02-17 09:14:16 +0300

    project start

---
+++ CodeBreaker/CodeBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreakerApp.swift
+//  CodeBreaker
+//
+//  Created by abramovanto on 17.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
